9770f5691df110b064e5e22764a8a4d015b34658
Totoaragaz <totovisan7@yahoo.com>
2024-03-01 00:32:08 +0200

    Added multiple-game simulations and frontend validations.

--- src/main/java/com/example/memorygameai/Controllers/ResultsController.java
+++ src/main/java/com/example/memorygameai/Controllers/ResultsController.java
@@ -11,11 +11,40 @@ public class ResultsController {
     @RequestMapping("/results")
     public String results(@RequestParam(value = "pairNr", required = true) String pairNr,
                           @RequestParam(value = "gameNr", required = true) String gameNr,
-                          Model model) {
+                          Model model)
+    {
+        boolean invalidArguments = false;
+        try {
+            if (Integer.parseInt(pairNr) < 1 || Integer.parseInt(gameNr) < 1) {
+                invalidArguments = true;
+            }
+        } catch (Exception e){
+            invalidArguments = true;
+        }
 
+        if (invalidArguments) {
+            return "index";
+        } else {
+            model.addAttribute("pairNr", pairNr);
+            model.addAttribute("gameNr", gameNr);
+
+
+            int[] wins = simulateGames(Integer.parseInt(pairNr), Integer.parseInt(gameNr));
+            model.addAttribute("p1Wins", wins[1]);
+            model.addAttribute("p2Wins", wins[2]);
+            model.addAttribute("draws", wins[0]);
+
+            return "results";
+        }
+    }
+
+    private int[] simulateGames(int pairNr, int gameNr) {
+        int[] wins = new int[3];
         GameController gameController = new GameController();
-        int winner = gameController.playGame(Integer.parseInt(pairNr));
-        model.addAttribute("winner", winner);
-        return "results";
+        for (int i = 0; i < gameNr; i++) {
+            wins[gameController.playGame(pairNr)]++;
+        }
+
+        return wins;
     }
 }
